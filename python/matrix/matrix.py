4d0c22b3e680a7c1fa1146d6c305f3539d1ed209
Ana Schwendler <schwendler@gmail.com>
2020-06-19 09:49:49 +0200

    Refactor rows method

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -2,22 +2,18 @@ class Matrix:
     def __init__(self, matrix_string):
         self.rows = self.__define_rows(matrix_string)
         self.columns = self.__define_columns(matrix_string)
-        pass
 
     def row(self, index):
-        return self.rows[index]
+        return self.rows[index - 1]
 
     def column(self, index):
         return self.columns[index]
 
-
     def __define_rows(self, matrix_string):
         split_lines = matrix_string.splitlines()
-        rows = {}
-        j = 1
+        rows = []
         for i in split_lines:
-            rows[j] = list(map(int, i.split()))
-            j += 1
+            rows.append(list(map(int, i.split())))
         return rows
 
     def __define_columns(self, matrix_string):
@@ -27,8 +23,8 @@ class Matrix:
             split = i.split()
             k = 1
             for j in split:
-              if k not in columns:
-                  columns[k] = []
-              columns[k].append(int(j))
-              k += 1
+                if k not in columns:
+                    columns[k] = []
+                columns[k].append(int(j))
+                k += 1
         return columns

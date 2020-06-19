2c320de063e6a0cdc841ccdc10a16fde3985f709
Ana Schwendler <schwendler@gmail.com>
2020-06-19 11:06:09 +0200

    Sum the result of the difference check.
    Sum 1 if char are different, 0 if not, use boolean to sum up. This solution looks so nice. I was amazed and decided to also try.

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("The size of the strings don't match")
-    return sum(1 for a, b in zip(strand_a, strand_b) if a != b)
+    return sum(a != b for a, b in zip(strand_a, strand_b))

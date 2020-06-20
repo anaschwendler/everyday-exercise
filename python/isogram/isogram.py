62e05c42330fa9ed3d7e4456a343fd53c86c4c3e
Ana Schwendler <schwendler@gmail.com>
2020-06-20 19:03:12 +0200

    refactor for to skip non alpha characters

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,10 +1,10 @@
 def is_isogram(string):
-    filter_alphachars = ''.join([i for i in string if i.isalpha()])
-    normalize_string = filter_alphachars.lower()
+    normalize_string = string.lower()
     characters = []
     for i in normalize_string:
-        if i not in characters:
-            characters.append(i)
-        else:
-            return False
+        if i.isalpha():
+            if i not in characters:
+                characters.append(i)
+            else:
+                return False
     return True

fcf4e8580d801c734086ea1bb880f5a18d466297
Ana Schwendler <schwendler@gmail.com>
2020-06-20 19:27:36 +0200

    Fix black suggestions

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,3 +1,3 @@
 def is_isogram(string):
-    filter_alphachars = ''.join([i for i in string if i.isalpha()]).lower()
+    filter_alphachars = "".join([i for i in string if i.isalpha()]).lower()
     return len(filter_alphachars) == len(set(filter_alphachars))

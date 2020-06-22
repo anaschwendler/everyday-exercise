02744750c20665d278b7e1578e232d8a72427499
Ana Schwendler <schwendler@gmail.com>
2020-06-22 17:24:23 +0200

    Use counter to count words

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import re
 import string
 
@@ -6,10 +7,4 @@ def count_words(sentence):
     remove_punct = re.sub(r",|_", " ", sentence)
     split_sentence = remove_punct.lower().split()
     clean_sentence = [word.strip(string.punctuation) for word in split_sentence]
-    word_counter = {}
-    for word in clean_sentence:
-        if word not in word_counter:
-            word_counter[word] = 1
-        else:
-            word_counter[word] += 1
-    return word_counter
+    return dict(Counter(clean_sentence))

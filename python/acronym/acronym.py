f80cb5b74a1c148c9ffb93213a5fe877a472da3d
Ana Schwendler <schwendler@gmail.com>
2020-06-24 12:24:29 +0200

    Use join to get first letter of each word

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -3,7 +3,4 @@ import re
 
 def abbreviate(words):
     split_words = re.sub(r"-|_", " ", words).split()
-    abbreviated_string = ''
-    for word in split_words:
-        abbreviated_string += word[0].upper()
-    return abbreviated_string
+    return "".join(word[0].upper() for word in split_words)

6130cabdb16775e3a1483dff301b7edfb58c8a1a
Ana Schwendler <schwendler@gmail.com>
2020-07-01 13:16:21 +0200

    Nice solution removing for loop

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -13,12 +13,12 @@ class Luhn:
     def __double_second_digit(self, card_num):
         card_number_list = list(card_num[::-1])
         card_number_int_list = [int(i) for i in card_number_list]
-        for i in range(1, len(card_number_int_list), 2):
-            double_value = card_number_int_list[i] * 2
-            card_number_int_list[i] = (
-                double_value if double_value < 9 else double_value - 9
-            )
-        return card_number_int_list
+        doubled = [self.__double_digit(i) for i in card_number_int_list[1::2]]
+        return card_number_int_list[::2] + doubled
+
+    def __double_digit(self, digit):
+        doubled_value = digit * 2
+        return doubled_value if doubled_value < 9 else doubled_value - 9
 
     def __sum_card_values(self, doubled_card_numbers):
         return sum(doubled_card_numbers)

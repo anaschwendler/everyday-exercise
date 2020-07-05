0456e9d4a0a04f8e9f5ac1fb82049fce371c61f0
Ana Schwendler <schwendler@gmail.com>
2020-07-05 12:55:23 +0200

    First exercise for ruby

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 python_dependencies:
 	python -m pip install --upgrade pip
 	pip install -r python/requirements.txt
+
+ruby_dependencies:
+	sudo gem install bundler
+	bundle install --gemfile=ruby/Gemfile
---
+++ ruby/hello-world/hello_world.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Hello World' exercise in this file. Make the tests in
+# `hello_world_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/hello-world` directory.
+
+class HelloWorld
+  def self.hello
+    'Hello, World!'
+  end
+end

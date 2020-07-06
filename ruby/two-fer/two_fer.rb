1e8412da7fe523c2d16968bfe02dc02824149c3e
Ana Schwendler <schwendler@gmail.com>
2020-07-06 16:33:17 +0200

    Include two-fer practice

---
+++ ruby/two-fer/two_fer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Two Fer' exercise in this file. Make the tests in
+# `two_fer_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/two-fer` directory.
+
+class TwoFer
+  def self.two_fer(name = 'you')
+    "One for #{name}, one for me."
+  end
+end

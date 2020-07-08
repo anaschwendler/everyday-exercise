c2bb5ccf5e5036cfdbd9a7ad1e375c43ad5150b1
Ana Schwendler <schwendler@gmail.com>
2020-07-08 12:05:26 +0200

    Add acronym exercise

---
+++ ruby/acronym/acronym.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Acronym' exercise in this file. Make the tests in
+# `acronym_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/acronym` directory.
+
+module Acronym
+  def self.abbreviate(sentence)
+    sanitize_sentence = sentence.downcase.gsub(/[^a-z0-9\s]/, ' ')
+    split_sentence = sanitize_sentence.split
+    map_acronym = split_sentence.map { |i| i[0] }
+    map_acronym.join.upcase
+  end
+end
---
+++ ruby/acronym/acronym_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative 'acronym'
+
+# Common test data version: 1.7.0 cacf1f1
+class AcronymTest < Minitest::Test
+  def test_basic
+    # skip
+    assert_equal 'PNG', Acronym.abbreviate('Portable Network Graphics')
+  end
+
+  def test_lowercase_words
+    assert_equal 'ROR', Acronym.abbreviate('Ruby on Rails')
+  end
+
+  def test_punctuation
+    assert_equal 'FIFO', Acronym.abbreviate('First In, First Out')
+  end
+
+  def test_all_caps_word
+    assert_equal 'GIMP', Acronym.abbreviate('GNU Image Manipulation Program')
+  end
+
+  def test_punctuation_without_whitespace
+    assert_equal 'CMOS', Acronym.abbreviate('Complementary metal-oxide semiconductor')
+  end
+
+  def test_very_long_abbreviation
+    assert_equal 'ROTFLSHTMDCOALM',
+                 Acronym.abbreviate('Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me')
+  end
+
+  def test_consecutive_delimiters
+    assert_equal 'SIMUFTA', Acronym.abbreviate('Something - I made up from thin air')
+  end
+
+  def test_apostrophes
+    assert_equal 'TRNT', Acronym.abbreviate('The Road _Not_ Taken')
+  end
+end

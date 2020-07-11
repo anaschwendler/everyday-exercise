315763a5e9ca9c908de911999e6cbb4c0dde2fd5
Ana Schwendler <schwendler@gmail.com>
2020-07-11 09:41:10 +0200

    Add matching brackets exercise

---
+++ ruby/matching-brackets/matching_brackets.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Matching Brackets' exercise in this file. Make the tests in
+# `matching_brackets_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/matching-brackets` directory.
+
+CORRESPONDENCE = {
+  '(' => ')',
+  '[' => ']',
+  '{' => '}'
+}.freeze
+
+module Brackets
+  def self.paired?(entry)
+    entry_balance = []
+
+    entry.each_char do |c|
+      if CORRESPONDENCE.keys.include? c
+        entry_balance.append(c)
+      elsif CORRESPONDENCE.values.include? c
+        return false unless CORRESPONDENCE[entry_balance.last] == c
+
+        entry_balance.pop
+      end
+    end
+
+    entry_balance.empty?
+  end
+end

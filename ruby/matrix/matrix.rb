94fe7412a64dc50166e6ced8acb71cf1ca23d3be
Ana Schwendler <schwendler@gmail.com>
2020-07-11 08:49:05 +0200

    Refactor to transpose rows in columns method, cause ruby has a transpose method, so convenient, damn.

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -14,24 +14,11 @@ class Matrix
   attr_reader :matrix
 
   def rows
+    splitted_matrix = matrix.split("\n")
     splitted_matrix.map { |i| i.split.map(&:to_i) }
   end
 
   def columns
-    columns = []
-
-    range = rows.length - 1
-    (0..range).each do |index|
-      column_values = rows.map { |i| i[index] }
-      columns.append(column_values)
-    end
-
-    columns
-  end
-
-  private
-
-  def splitted_matrix
-    matrix.split("\n")
+    rows.transpose
   end
 end

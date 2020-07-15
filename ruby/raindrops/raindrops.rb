a8f750f930cd8d584c187721215ee0f8a3048224
Ana Schwendler <schwendler@gmail.com>
2020-07-15 14:36:33 +0200

    Add Raindrops exercise

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Raindrops' exercise in this file. Make the tests in
+# `raindrops_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/raindrops` directory.
+
+DIVISORS = {
+  3 => 'Pling',
+  5 => 'Plang',
+  7 => 'Plong'
+}.freeze
+
+module Raindrops
+  def self.convert(digit)
+    raindrops = ''
+
+    DIVISORS.keys.each do |d|
+      raindrops += DIVISORS[d] if (digit % d).zero?
+    end
+
+    raindrops.empty? ? digit.to_s : raindrops
+  end
+end

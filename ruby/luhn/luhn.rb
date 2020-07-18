cf956ec2755ba8cb829cb9a6ecae570e090841ee
Ana Schwendler <schwendler@gmail.com>
2020-07-18 19:20:06 +0200

    Refactor a bit the luhn method

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -11,22 +11,29 @@ module Luhn
     parsed_number = card_number.delete(' ')
     return false if invalid_card(parsed_number)
 
-    summed_array = double_array(parsed_number).sum
-    (summed_array % 10).zero?
+    (sum_array(parsed_number) % 10).zero?
+  end
+
+  def self.invalid_card(card_number)
+    card_number.length == 1 || !(card_number =~ /[^0-9]/).nil?
   end
 
   def self.double_array(parsed_number)
     arrayed_number = parsed_number.reverse.chars.map(&:to_i)
     iteraction_length = arrayed_number.length - 1
     (1..iteraction_length).step(2).each do |i|
-      double_number = arrayed_number[i] * 2
-      arrayed_number[i] = double_number > 9 ? double_number - 9 : double_number
+      arrayed_number[i] = double_number(arrayed_number[i])
     end
 
     arrayed_number
   end
 
-  def self.invalid_card(card_number)
-    card_number.length == 1 || !(card_number =~ /[^0-9]/).nil?
+  def self.double_number(number)
+    double_number = number * 2
+    double_number > 9 ? double_number - 9 : double_number
+  end
+
+  def self.sum_array(parsed_number)
+    double_array(parsed_number).sum
   end
 end

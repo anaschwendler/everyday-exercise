4676a1e831e776896ff54b13d5de93d46bd4b8ec
Ana Schwendler <schwendler@gmail.com>
2020-08-18 12:10:11 +0200

    Add solution without recursion

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -6,32 +6,46 @@
 # To get started with TDD, see the `README.md` file in your
 # `ruby/grains` directory.
 
+## LESSONS
+## I wanted to study recursion with this exercise, so I'll leave both solutions, with and without recursion
+
 BOARD_SIZE = 64
 
 module Grains
   def self.square(size)
     raise ArgumentError, "Board size should be between 1 and #{BOARD_SIZE}" unless size.between?(1, BOARD_SIZE)
 
-    calculate_square(size, 1, 1)
+    power = size - 1
+    2**power
   end
 
   def self.total
-    sum_grains(BOARD_SIZE, 1, 1)
+    (1..BOARD_SIZE).sum { |n| square(n) }
   end
 
-  def self.calculate_square(total_size, actual_position, actual_size)
-    if total_size == actual_position
-      actual_size
-    else
-      calculate_square(total_size, actual_position + 1, actual_size * 2)
-    end
-  end
-
-  def self.sum_grains(total_size, actual_position, actual_size)
-    if total_size == actual_position
-      actual_size
-    else
-      actual_size + sum_grains(total_size, actual_position + 1, actual_size * 2)
-    end
-  end
+  # def self.square(size)
+  #   raise ArgumentError, "Board size should be between 1 and #{BOARD_SIZE}" unless size.between?(1, BOARD_SIZE)
+  #
+  #   calculate_square(size, 1, 1)
+  # end
+  #
+  # def self.total
+  #   sum_grains(BOARD_SIZE, 1, 1)
+  # end
+  #
+  # def self.calculate_square(total_size, actual_position, actual_size)
+  #   if total_size == actual_position
+  #     actual_size
+  #   else
+  #     calculate_square(total_size, actual_position + 1, actual_size * 2)
+  #   end
+  # end
+  #
+  # def self.sum_grains(total_size, actual_position, actual_size)
+  #   if total_size == actual_position
+  #     actual_size
+  #   else
+  #     actual_size + sum_grains(total_size, actual_position + 1, actual_size * 2)
+  #   end
+  # end
 end

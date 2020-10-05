0e0e2c7df98c50f8974daa9e265c8095ef908d6b
Ana Schwendler <ana.schwendler@marleyspoon.com>
2020-10-05 13:39:39 +0200

    Refactoring :cake:
    Okay I didn't need to go so far for a solution

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -8,23 +8,12 @@
 
 module Pangram
   def self.pangram?(sentence)
-    counter = create_letter_counter
+    return false if sentence.empty?
 
-    sentence.downcase.chars.each do |letter|
-      counter[letter] += 1 if counter.key?(letter)
-    end
-
-    not_used_letters = counter.select { |_key, value| value.zero? }
-
-    not_used_letters.empty?
-  end
-
-  def self.create_letter_counter
-    counter = {}
     ('a'..'z').each do |letter|
-      counter[letter] = 0
+      return false unless sentence.downcase.include? letter
     end
 
-    counter
+    true
   end
 end

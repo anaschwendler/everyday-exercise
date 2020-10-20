c52992111179a39a9ff53effa9f8196c95b861b1
Ana Schwendler <ana.schwendler@marleyspoon.com>
2020-10-20 15:47:59 +0200

    Refactor luhn exercise :sparkles:

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -7,19 +7,27 @@
 # `ruby/luhn` directory.
 
 module Luhn
-  def self.valid?(card_number)
-    parsed_number = card_number.delete(' ')
-    return false if invalid_card(parsed_number)
+  def self.valid?(number)
+    parsed_number = number.delete(' ')
+    return false if invalid_sequence?(parsed_number)
 
-    (sum_array(parsed_number) % 10).zero?
+    parsed_sequence = prepare_sequence(parsed_number)
+    double_digits = double_array(parsed_sequence)
+    sum_sequence = double_digits.sum
+
+    valid_sequence?(sum_sequence)
   end
 
-  def self.invalid_card(card_number)
-    card_number.length == 1 || !(card_number =~ /[^0-9]/).nil?
+  def self.invalid_sequence?(parsed_number)
+    parsed_number.length == 1 || !(parsed_number =~ /[^0-9]/).nil?
+  end
+
+  def self.prepare_sequence(parsed_number)
+    parsed_number.reverse.chars.map(&:to_i)
   end
 
   def self.double_array(parsed_number)
-    arrayed_number = parsed_number.reverse.chars.map(&:to_i)
+    arrayed_number = parsed_number
     iteraction_length = arrayed_number.length - 1
     (1..iteraction_length).step(2).each do |i|
       arrayed_number[i] = double_number(arrayed_number[i])
@@ -33,7 +41,7 @@ module Luhn
     double_number > 9 ? double_number - 9 : double_number
   end
 
-  def self.sum_array(parsed_number)
-    double_array(parsed_number).sum
+  def self.valid_sequence?(sequence)
+    (sequence % 10).zero?
   end
 end
